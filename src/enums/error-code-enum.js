ae298215171f78cd50c57388eabd9047eec69ece
Sahan Chamara <sahanchamara456@gmail.com>
2025-05-19 23:30:02 +0530

    httpConfig and error code cofigs

---
+++ src/enums/error-code-enum.js
@@ -0,0 +1,13 @@
+export const ErrorCodeEnum = {
+    AUTH_NOT_FOUND: "AUTH_NOT_FOUND",
+
+    //Access Control Error
+    ACCESS_UNAUTHORIZED: "ACCESS_UNAUTHORIZED",
+
+    // Validation and resource Error
+    VALIDATION_ERROR: "VALIDATION_ERROR",
+    RESOURCE_NOT_FOUND: "RESOURCE_NOT_FOUND",
+
+    //System Error
+    INTERNAL_SERVER_ERROR:  "INTERNAL_SERVER_ERROR",
+};
